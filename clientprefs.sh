7bfdd0e09576a9db55320eb887e0725ff80370d0
adamphetamine <47110465+adamphetamine@users.noreply.github.com>
2023-02-08 10:43:28 +1100

    Update clientprefs.sh
    this version with many thanks to my AI overlord

--- clientprefs.sh
+++ clientprefs.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
+#
+# How to use this script
+# download and make executable with chmod +x
+# Example usage- sudo sh ./clientprefs.sh "YumBrands" "KFC" "Chook_Cook" "Sydney" "MyCustomvalue"
 
-# Example- sudo sh ./clientprefs.sh "YumBrands" "KFC" "Chook_Cook" "Sydney" "MyCustomvalue"
-VERSION=1.00
+# Get the host operating system information
+OS=$(uname -s)
+
+# Print the result
+echo "The host operating system is: $OS"
+
+# Define the input arguments
+VERSION=1.00   # yes this is hard coded 
 COMPANYGROUP="$1"
 COMPANY="$2"
 BUSINESSUNIT="$3"
 LOCATION="$4"
 CUSTOM1="$5"
 
-cat > com.servicemax.clientprefs.plist <<EOF
-<?xml version="1.0" encoding="UTF-8"?>
+# Define the contents of the .plist file
+plist_contents="<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-        <key>CompanyGroup</key>
-        <string>$COMPANYGROUP</string>
-        <key>Company</key>
-        <string>$COMPANY</string>
-        <key>BusinessUnit</key>
-        <string>$BUSINESSUNIT</string>
-        <key>Location</key>
-        <string>$LOCATION</string>
-        <key>Custom1</key>
-        <string>$CUSTOM1</string>
+    <key>COMPANYGROUP</key>
+    <string>$COMPANYGROUP</string>
+    <key>COMPANY</key>
+    <string>$COMPANY</string>
+    <key>BUSINESSUNIT</key>
+    <string>$BUSINESSUNIT</string>
+    <key>LOCATION</key>
+    <string>$LOCATION</string>
+    <key>CUSTOM1</key>
+    <string>$CUSTOM1</string>
 </dict>
-</plist>
-EOF
+</plist>"
+
+# Determine the target directory based on the host operating system
+if [ "$OS" == "Darwin" ]; then
+  target_dir="/Application Support/Servicemax"
+elif [ "$OS" == "Linux" ]; then
+  target_dir="/opt"
+else
+  echo "Unsupported operating system: $OS"
+  exit 1
+fi
+
+# Create the target directory if it doesn't exist
+if [ ! -d "$target_dir" ]; then
+  mkdir -p "$target_dir"
+fi
+
+# Define the path to the .plist file
+plist_path="$target_dir/com.servicemax.clientprefs.plist"
+
+# Write the contents to a file
+echo "$plist_contents" > "$plist_path"
+
+echo finished writing preferences to file
